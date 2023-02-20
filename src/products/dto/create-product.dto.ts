a526997a8dc9106812800171f80f81725a6ce061
Lucas Garzon <garzon.lucas.m@gmail.com>
2023-02-20 02:05:03 -0300

    refactor: products => documentación actualizada

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProductDto {
+  @ApiProperty({ example: 'Producto 01', description: 'The name of a product' })
   name: string;
+  @ApiProperty({ example: 10, description: 'The price of a product' })
   price: number;
+  @ApiProperty({ example: 250, description: 'The stock of a product' })
   stock: number;
+  @ApiProperty({
+    example:
+      'https://i.pinimg.com/originals/24/58/5f/24585fc9b7433a224a6ff5506e346969.png',
+    description: 'The photo of a product',
+  })
   thumbnail: string;
 }
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,9 +1,17 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiProperty({ example: 'Producto 01', description: 'The name of a product' })
   name: string;
+  @ApiProperty({ example: 10, description: 'The price of a product' })
   price: number;
+  @ApiProperty({ example: 250, description: 'The stock of a product' })
   stock: number;
+  @ApiProperty({
+    example:
+      'https://i.pinimg.com/originals/24/58/5f/24585fc9b7433a224a6ff5506e346969.png',
+    description: 'The photo of a product',
+  })
   thumbnail: string;
 }
